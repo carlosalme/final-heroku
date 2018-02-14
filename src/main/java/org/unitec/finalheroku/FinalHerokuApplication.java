e181baac0267beafa9a8998ba823b6dc3286dd0b
T-107 <T-107@PC200>
2018-02-13 21:20:27 -0600

    primera actualizacion

---
+++ src/main/java/org/unitec/finalheroku/FinalHerokuApplication.java
@@ -0,0 +1,12 @@
+package org.unitec.finalheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalHerokuApplication.class, args);
+	}
+}
---
+++ src/test/java/org/unitec/finalheroku/FinalHerokuApplicationTests.java
@@ -0,0 +1,16 @@
+package org.unitec.finalheroku;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FinalHerokuApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
